7631b042f4ecfd5314c7bc25ef3911eeb1fdca7c
MisaelSalazar <misaelsalazarof@protonmail.com>
2025-05-07 15:48:45 -0600

    cors

--- endpoints/login.php
+++ endpoints/login.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+require_once '../config/cors.php';
 require_once '../config/database.php';
 
 $data = json_decode(file_get_contents("php://input"));
